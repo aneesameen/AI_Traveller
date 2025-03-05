3a482dd01bfa2bb9ddd4417982858e0edad9e027
aneesameen <aneesameen00@gmail.com>
2025-03-05 20:11:29 +0530

    revert again

--- Backend/routes/Stripe.js
+++ Backend/routes/Stripe.js
@@ -10,8 +10,7 @@ router.post('/create-checkout-session', async (req, res) => {
 
     try {
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card', 'google_pay'], // Add Google Pay explicitly
-            // payment_method_types: ['card'],
+            payment_method_types: ['card'],
             line_items: [
                 {
                     price_data: {
