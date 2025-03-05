9ffb57ffa86edf59bd1cfe9b7c55b2aafb8cc8d1
aneesameen <aneesameen00@gmail.com>
2025-03-05 19:41:36 +0530

    added g-pay

--- Backend/routes/Stripe.js
+++ Backend/routes/Stripe.js
@@ -10,7 +10,7 @@ router.post('/create-checkout-session', async (req, res) => {
 
     try {
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
+            payment_method_types: ['card', 'google_pay'],
             line_items: [
                 {
                     price_data: {
